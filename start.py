c2f13893f422993aa780014d89d4f310653beb79
Jack Holmes <jackholmes5194@gmail.com>
2019-08-25 17:06:36 +1200

    making progress

--- start.py
+++ start.py
@@ -14,16 +14,59 @@ def main(stdscr):
     # Keep track of (y, x) coordinates
     positions = []
 
+    sentence = list('Hello world')
+    sentence.reverse()
+    next_char = sentence.pop()
+    correctly_typed_character_count = 0
+    incorrectly_typed_character_count = 0
+
+    incorrect_progress_y = 6
+    incorrect_progress_x = 6
+
+    correct_progress_y = 5
+    correct_progress_x = 6
+
+    def update_incorrect_progress():
+        (pos_y, pos_x) = stdscr.getyx()
+        stdscr.move(incorrect_progress_y, incorrect_progress_x)
+        stdscr.clrtoeol()
+        stdscr.addstr('Incorrect: {}'.format(str(incorrectly_typed_character_count)))
+        stdscr.move(pos_y, pos_x)
+
+    def update_correct_progress():
+        (pos_y, pos_x) = stdscr.getyx()
+        stdscr.move(correct_progress_y, correct_progress_x)
+        stdscr.clrtoeol()
+        stdscr.addstr('Correct: {}'.format(str(correctly_typed_character_count)))
+        stdscr.move(pos_y, pos_x)
+
     while True:
         key = stdscr.getkey()
 
         if (key == 'KEY_BACKSPACE'):
             if (len(positions) > 0):
-                (previous_pos_y, previous_pos_x) = positions.pop()
-                stdscr.delch(previous_pos_y, previous_pos_x)
+                if (incorrectly_typed_character_count > 0):
+                    incorrectly_typed_character_count -= 1
+                    update_incorrect_progress()
+                    (previous_pos_y, previous_pos_x) = positions.pop()
+                    stdscr.delch(previous_pos_y, previous_pos_x)
+
         else:
+            # All characters have been typed
+            if (len(sentence) == 0):
+                break
+
             positions.append(stdscr.getyx())
             stdscr.addstr(key)
+
+            # Check if character was typed correctly
+            if (incorrectly_typed_character_count == 0 and key == next_char):
+                next_char = sentence.pop()
+                correctly_typed_character_count += 1
+                update_correct_progress()
+            else:
+                incorrectly_typed_character_count += 1
+                update_incorrect_progress()
 
         stdscr.refresh()
 

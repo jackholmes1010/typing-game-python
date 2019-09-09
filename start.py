483a1cc86b763e7552dc69569dffad209cf2b3ab
Jack Holmes <jackholmes5194@gmail.com>
2019-09-09 17:16:07 +0100

    save

--- start.py
+++ start.py
@@ -44,7 +44,7 @@ def main(stdscr):
         sentence_window_y,
         sentence_window_x,
     )
-    sentence_window.border('|', '|', '-', '-', '+', '+', '+', '+')
+    sentence_window.border("|", "|", "-", "-", "+", "+", "+", "+")
     sentence_window.addstr(1, 1, sentence.get_sentence(), curses.A_DIM)
     sentence_window.move(1, 1)
     sentence_window.keypad(1)
@@ -52,19 +52,21 @@ def main(stdscr):
     while True:
         key = sentence_window.getkey()
 
-        if (key == "KEY_BACKSPACE"):
-            if (len(cursor_positions) < 1):
+        if key == "KEY_BACKSPACE":
+            if len(cursor_positions) < 1:
                 continue
 
             # Remove previous character
             (previous_pos_y, previous_pos_x) = cursor_positions.pop()
             overwritten_char = overwritten_chars.pop()
-            sentence_window.addstr(previous_pos_y, previous_pos_x, overwritten_char)
+            sentence_window.addstr(
+                previous_pos_y, previous_pos_x, overwritten_char
+            )
             sentence_window.move(previous_pos_y, previous_pos_x)
 
             # If there are incorrect characters on
             # screen, backspacing must be to remove these.
-            if (incorrect_characters_on_screen > 0):
+            if incorrect_characters_on_screen > 0:
                 incorrect_characters_on_screen -= 1
             # If there are no incorrect characters on screen,
             # we must be removing correctly typed characters.
@@ -73,14 +75,16 @@ def main(stdscr):
         else:
             # Keep track of characters that have been overwritten
             (current_pos_y, current_pos_x) = sentence_window.getyx()
-            char_at_cursor = sentence_window.instr(current_pos_y, current_pos_x, 1)
+            char_at_cursor = sentence_window.instr(
+                current_pos_y, current_pos_x, 1
+            )
             cursor_positions.append((current_pos_y, current_pos_x))
             overwritten_chars.append(char_at_cursor)
 
             # Typed character cannot be correct if there are
             # incorrectly typed characters currently on screen.
-            if (key == next_char and incorrect_characters_on_screen <= 0):
-                if (sentence.has_more_chars() == False):
+            if key == next_char and incorrect_characters_on_screen <= 0:
+                if sentence.has_more_chars() == False:
                     print("Success")
                     break
 

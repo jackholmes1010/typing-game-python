9dd0dd6f9f65363adf8a75def7740af285492dcc
Jack Holmes <jackholmes5194@gmail.com>
2019-09-10 10:54:40 +0100

    remove

--- Helper.py
+++ Helper.py
@@ -1,6 +1,3 @@
-# Each sentence has correct parts, un typed parts
-# Backspacing gives back the original char
-# Processing key gives correct or false
 class Helper:
     def __init__(self, sentence = ''):
         if (sentence == ''):
@@ -34,8 +31,11 @@ class Helper:
 
         return self.sentence[self.index]
 
+    def has_more_characters(self):
+        return self.index < len(self.sentence)
+
     def get_sentence(self):
         return self.sentence
 
     def generate_sentence(self):
-        return "Herp derpsum tee sherper perp derps derperker derpler ler merpus nerpy. Sherper perper derpsum herpderpsmer derperker, me merpus nerpy herpsum. Nerpy perp herp berps sherp terpus derpler. Derps perp ter serp derpy herpler herderder derpsum. Derpus herpy derp derps derpy herp merp berps nerpy. Herderder re derpsum, sherper derperker! Herpy merp nerpy merpus. Re derp ner derperker se sherlamer herpy ze cerp tee. Sherper herpsum er berp serp derpler derpus terpus. Derp herpderpsmer, merp berp perper herderder sherpus sherper herpy. Re herp herpler herpderpsmer sherlamer derperker dee derps. Sherpus herpler derperker perper re perp se ter sherp. Derps herpem herderder pee herpsum terpus sherlamer jerpy herpler? Derps de herpsum me herpderpsmer berps derpsum herderder, ner derpy?"
+        return "Herp derpsum tee sherper perp derps derperker derpler ler merpus nerpy. Sherper perper derpsum herpderpsmer derperker, me merpus nerpy herpsum."
--- start2.py
+++ start2.py
@@ -9,15 +9,14 @@ curses.cbreak()
 curses.curs_set(True)
 curses.resize_term(100, 50)
 
-# Keep track of cursor positions
-cursor_positions = []
-
-helper = Helper()
-
-
 def main(stdscr):
+    helper = Helper()
+    cursor_positions = []
+    incorrect_character_count = 0
+
     stdscr.clear()
     stdscr.addstr(helper.get_sentence(), curses.A_LOW)
+    stdscr.move(0, 0)
 
     while True:
         key = stdscr.getkey()
@@ -26,6 +25,7 @@ def main(stdscr):
             if len(cursor_positions) < 1:
                 continue
 
+            # Restore overwritten characters
             process_backspace_result = helper.process_backspace()
             previous_pos = cursor_positions.pop()
             stdscr.move(previous_pos[0], previous_pos[1])
@@ -37,15 +37,26 @@ def main(stdscr):
                 stdscr.addstr(process_backspace_result)
 
             stdscr.move(pos[0], pos[1])
+
+            # If there are incorrect characters on
+            # screen, backspacing must be to remove these.
+            if incorrect_character_count > 0:
+                incorrect_character_count -= 1
+
         else:
             pos = stdscr.getyx()
             cursor_positions.append(pos)
             process_key_result = helper.process_key(key)
 
-            if process_key_result == True:
+            if process_key_result == True and incorrect_character_count <= 0:
                 stdscr.addstr(key, curses.A_BOLD)
+
+                if helper.has_more_characters() == False:
+                    print("Success")
+                    break
             else:
                 stdscr.addstr(key, curses.A_UNDERLINE)
+                incorrect_character_count += 1
 
         curses.napms(10)
 

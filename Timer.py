805aeee3d41135d6bef5ae4fe6ed097fa442d7a3
Jack Holmes <jackholmes5194@gmail.com>
2019-09-10 12:53:04 +0100

    refactorying

--- Timer.py
+++ Timer.py
@@ -1,52 +1,33 @@
 import time
 
 
-class Timer:
-    def __init__(self):
-        self.start = time.time()
-        self.elapsed = None
-
-    def tick(self):
-        if (self.elapsed == None):
-            self.elapsed = time.time()
-            return 0.0
-        else:
-            now = time.time()
-            elapsed = now - self.elapsed
-            self.elapsed = now
-            return elapsed
-
-    def stop(self):
-        return time.time() - self.start
-
 class Metrics:
     def __init__(self):
         self.start = time.time()
         self.tick = time.time()
         self.count = 1
-        self.cpm = 0
+        self.wpm = 0
         self.total_characters_typed = 0
 
-    def current_cpm(self):
+    def current_wpm(self):
         self.total_characters_typed += 1
 
         if self.count % 5 == 0:
             elapsed = self.get_elapsed_time()
-            self.cpm = self.total_characters_typed / elapsed
+            self.wpm = self.total_characters_typed / elapsed / 5
             self.count = 1
             self.tick = time.time()
-            return self.cpm
+            return self.wpm
         else:
             self.count += 1
-            return self.cpm
+            return self.wpm
 
-    def overall_cpm(self):
+    def overall_wpm(self):
         elapsed = self.get_elapsed_time()
-        return self.total_characters_typed / elapsed
+        return self.total_characters_typed / elapsed / 5
 
     def get_elapsed_time(self):
         current_time_minutes = time.time() / 60
         start_time_minutes = self.start / 60
         elapsed_time_minutes = current_time_minutes - start_time_minutes
         return elapsed_time_minutes
-
--- start.py
+++ start.py
@@ -31,18 +31,16 @@ class Game:
 
     def push_cursor_position(self):
         "Save current cursor position."
-        self.position = self.stdscr.getyx()
+        cursor_position = self.stdscr.getyx()
+        self.cursor_positions.append(cursor_position)
 
     def pop_cursor_position(self):
         "Move to previous cursor position."
-        self.stdscr.move(self.position[0], self.position[1])
+        if len(self.cursor_positions) > 0:
+            previous_cursor_position = self.cursor_positions.pop()
+            self.stdscr.move(previous_cursor_position[0], previous_cursor_position[1])
 
-    def move_cursor_to_previous_position(self):
-        if (len(self.cursor_positions) > 0):
-            previous_pos = self.cursor_positions.pop()
-            self.stdscr.move(previous_pos[0], previous_pos[1])
-
-    def main(self):
+    def start_game_loop(self):
         while True:
             key = self.stdscr.getkey()
 
@@ -51,7 +49,7 @@ class Game:
                     continue
 
                 result = self.helper.process_backspace()
-                self.move_cursor_to_previous_position()
+                self.pop_cursor_position()
                 self.push_cursor_position()
                 self.stdscr.addstr(result)
                 self.pop_cursor_position()
@@ -59,22 +57,26 @@ class Game:
                 if self.incorrect_character_count > 0:
                     self.incorrect_character_count -= 1
             else:
-                pos = self.stdscr.getyx()
-                self.cursor_positions.append(pos)
+                self.push_cursor_position()
+
                 result = self.helper.process_key(key)
 
                 if result == True and self.incorrect_character_count <= 0:
                     self.stdscr.addstr(key, curses.A_BOLD)
                     self.push_cursor_position()
-                    current_cpm = self.metrics.current_cpm()
-                    current_wpm = current_cpm / 5
-                    self.stdscr.addstr(9, 0, "WPM: {}".format(current_wpm))
+                    self.stdscr.addstr(
+                        10, 0, "WPM: {}".format(self.metrics.current_wpm())
+                    )
                     self.pop_cursor_position()
 
                     if self.helper.has_more_characters() == False:
-                        overall_cpm = self.metrics.overall_cpm()
-                        overall_wpm = overall_cpm / 5
-                        self.stdscr.addstr(10, 0, "WPM: {}. Press any key to exit...".format(overall_wpm))
+                        self.stdscr.addstr(
+                            10,
+                            0,
+                            "WPM: {}. Press any key to exit...".format(
+                                self.metrics.overall_wpm()
+                            ),
+                        )
                         key = self.stdscr.getkey()
                         break
                 else:
@@ -83,8 +85,10 @@ class Game:
 
             curses.napms(10)
 
+
 def main(stdscr):
     game = Game(stdscr)
-    game.main()
+    game.start_game_loop()
+
 
 wrapper(main)

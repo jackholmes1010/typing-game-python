d45a353141af32ab466298b3a98fa93bb4450d59
Jack Holmes <jackholmes5194@gmail.com>
2019-09-10 12:44:59 +0100

    refactoring

--- start.py
+++ start.py
@@ -4,74 +4,87 @@ from curses import wrapper
 from Helper import Helper
 from Timer import Metrics
 
-stdscr = curses.initscr()
-stdscr.keypad(True)
-curses.noecho()
-curses.cbreak()
-curses.curs_set(True)
-curses.resize_term(100, 60)
 
-class Positions:
-    def push_cursor_position(self, stdscr):
-        self.position = stdscr.getyx()
+class Game:
+    def __init__(self, stdscr):
+        self.stdscr = stdscr
+        self.helper = Helper()
+        self.metrics = Metrics()
+        self.cursor_positions = []
+        self.incorrect_character_count = 0
+        self.init_screen()
+        self.init_curses()
+        self.push_cursor_position()
 
-    def pop_cursor_position(self, stdscr):
-        stdscr.move(self.position[0], self.position[1])
+    def init_curses(self):
+        curses.noecho()
+        curses.cbreak()
+        curses.curs_set(True)
+        curses.resize_term(100, 60)
+
+    def init_screen(self):
+        "Initialize screen."
+        self.stdscr.keypad(True)
+        self.stdscr.clear()
+        self.stdscr.addstr(self.helper.get_sentence(), curses.A_LOW)
+        self.stdscr.move(0, 0)
+
+    def push_cursor_position(self):
+        "Save current cursor position."
+        self.position = self.stdscr.getyx()
+
+    def pop_cursor_position(self):
+        "Move to previous cursor position."
+        self.stdscr.move(self.position[0], self.position[1])
+
+    def move_cursor_to_previous_position(self):
+        if (len(self.cursor_positions) > 0):
+            previous_pos = self.cursor_positions.pop()
+            self.stdscr.move(previous_pos[0], previous_pos[1])
+
+    def main(self):
+        while True:
+            key = self.stdscr.getkey()
+
+            if key == "KEY_BACKSPACE":
+                if len(self.cursor_positions) < 1:
+                    continue
+
+                result = self.helper.process_backspace()
+                self.move_cursor_to_previous_position()
+                self.push_cursor_position()
+                self.stdscr.addstr(result)
+                self.pop_cursor_position()
+
+                if self.incorrect_character_count > 0:
+                    self.incorrect_character_count -= 1
+            else:
+                pos = self.stdscr.getyx()
+                self.cursor_positions.append(pos)
+                result = self.helper.process_key(key)
+
+                if result == True and self.incorrect_character_count <= 0:
+                    self.stdscr.addstr(key, curses.A_BOLD)
+                    self.push_cursor_position()
+                    current_cpm = self.metrics.current_cpm()
+                    current_wpm = current_cpm / 5
+                    self.stdscr.addstr(9, 0, "WPM: {}".format(current_wpm))
+                    self.pop_cursor_position()
+
+                    if self.helper.has_more_characters() == False:
+                        overall_cpm = self.metrics.overall_cpm()
+                        overall_wpm = overall_cpm / 5
+                        self.stdscr.addstr(10, 0, "WPM: {}. Press any key to exit...".format(overall_wpm))
+                        key = self.stdscr.getkey()
+                        break
+                else:
+                    self.stdscr.addstr(key, curses.A_UNDERLINE)
+                    self.incorrect_character_count += 1
+
+            curses.napms(10)
 
 def main(stdscr):
-    helper = Helper()
-    positions = Positions()
-    metrics = Metrics()
-    cursor_positions = []
-    incorrect_character_count = 0
-    num_chars_typed = 0
-
-    stdscr.clear()
-    stdscr.addstr(helper.get_sentence(), curses.A_LOW)
-    stdscr.move(0, 0)
-
-    while True:
-        key = stdscr.getkey()
-
-        if key == "KEY_BACKSPACE":
-            if len(cursor_positions) < 1:
-                continue
-
-            result = helper.process_backspace()
-            previous_pos = cursor_positions.pop()
-            stdscr.move(previous_pos[0], previous_pos[1])
-            pos = stdscr.getyx()
-            stdscr.addstr(result)
-            stdscr.move(pos[0], pos[1])
-
-            if incorrect_character_count > 0:
-                incorrect_character_count -= 1
-        else:
-            pos = stdscr.getyx()
-            cursor_positions.append(pos)
-            result = helper.process_key(key)
-
-            if result == True and incorrect_character_count <= 0:
-                stdscr.addstr(key, curses.A_BOLD)
-                num_chars_typed += 1
-
-                positions.push_cursor_position(stdscr)
-                current_cpm = metrics.current_cpm()
-                current_wpm = current_cpm / 5
-                stdscr.addstr(9, 0, "WPM: {}".format(current_wpm))
-                positions.pop_cursor_position(stdscr)
-
-                if helper.has_more_characters() == False:
-                    overall_cpm = metrics.overall_cpm()
-                    overall_wpm = overall_cpm / 5
-                    stdscr.addstr(10, 0, "WPM: {}. Press any key to exit...".format(overall_wpm))
-                    key = stdscr.getkey()
-                    break
-            else:
-                stdscr.addstr(key, curses.A_UNDERLINE)
-                incorrect_character_count += 1
-
-        curses.napms(10)
-
+    game = Game(stdscr)
+    game.main()
 
 wrapper(main)
